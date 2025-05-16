0051a2f953ef2a162f8bf8d663b8a65414151e9f
Stella Sikhila <stellavinmaris@gmail.com>
2025-05-16 15:12:45 +0300

    fixed canonical

--- src/components/CanonicalHeaders.jsx
+++ src/components/CanonicalHeaders.jsx
@@ -2,18 +2,24 @@ import React from 'react';
 import { Helmet } from 'react-helmet-async';
 import { useLocation } from 'react-router-dom';
 
-const CanonicalHeaders = ({ title, description }) => {
-  const location = useLocation();
-  const baseUrl = `${window.location.protocol}//${window.location.host}`;
-  const canonicalUrl = `${baseUrl}${location.pathname}${location.search}`;
+const CanonicalHeaders = ({title, description}) => {
 
-  return (
-    <Helmet>
-      <title>{title}</title>
-      <meta name="description" content={description} />
-      <link rel="canonical" href={canonicalUrl} />
-    </Helmet>
-  );
+    const location = useLocation();
+    let canonicalUrl = `https://www.camitrade.com${location.pathname}`;
+
+    if(location.search) {
+        canonicalUrl = `https://www.camitrade.com${location.pathname}${location.search}`;
+    } 
+
+    return (
+        <>
+         <Helmet>
+                <title>{title}</title>
+                <meta name="description" content={description}/>
+                <link rel="canonical" href={canonicalUrl} />
+        </Helmet>
+        </>
+    );
 };
 
 export default CanonicalHeaders;
